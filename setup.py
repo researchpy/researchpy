27995717b4e9365a6acce19f89c3469d43739389
Guy Taylor <thebigguy.co.uk@gmail.com>
2020-01-08 18:48:35 +0000

    Allow method="pearson" for corr_case and corr_pair
    At current corr_(case|pair)(df, method="pearson") fails even though it a
    supported algorithm. This enables it to be defined as well as supporting
    the previous "None" value. It also gives a better error message if an
    unknown method name is given.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Last updated: 02/18/2019
 from setuptools import setup
 
 setup(name= "researchpy",
-      version= "0.1.8",
+      version= "0.1.9",
       description= "researchpy produces Pandas DataFrames that contain relevant statistical testing information that is commonly required for academic research.",
       long_description= "researchpy produces Pandas DataFrames that contain relevant statistical testing information that is commonly required for academic research. The information is returned as Pandas DataFrames to make for quick and easy exporting of results to any format/method that works with the traditional Pandas DataFrame. researchpy is essentially a wrapper that combines various established packages such as pandas, scipy.stats, and statsmodels to get all the standard required information in one method. If analyses were not available in these packages, code was developed to fill the gap.",
       url= "http://researchpy.readthedocs.io/",
