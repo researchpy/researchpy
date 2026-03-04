21e7f84025a103b97cf2afc9c041b7f1ff5656d5
Corey Bryant <services.cb10@protonmail.com>
2026-03-04 15:17:17 -0500

    * Updated crosstab.py to address error while creating results tables for new versions of Pandas. Switch to constructing a dictionary first, then converting the dictionary to a Pandas DataFrame.

--- researchpy/crosstab.py
+++ researchpy/crosstab.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug  2 10:36:11 2018
+Created on Thursday, August  2, 2018
+@author: Corey Bryant
 
-@author: CoreySSD
+Last updated on Wednesday, March 4, 2026
+Updated by @author: Corey Bryant
 """
 
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 import scipy.stats
 from statsmodels.stats import contingency_tables
 
-def crosstab(group1, group2, prop= None, test = False, margins= True,
-             correction = None, cramer_correction = None, exact = False, expected_freqs= False):
+def crosstab(group1, group2, prop=None, test=False, margins=True,
+             correction=None, cramer_correction=None, exact=False, expected_freqs=False):
 
-    if type(group1) != pandas.core.series.Series or type(group2) != pandas.core.series.Series:
+    if type(group1) != pd.core.series.Series or type(group2) != pd.core.series.Series:
         return "Operation only supports Pandas Series"
 
     else:
         ## Creating the crosstab table ##
-        crosstab = pandas.crosstab(group1, group2)
-        crosstab2 = pandas.crosstab(group1, group2, margins = True)
+        crosstab = pd.crosstab(group1, group2)
+        crosstab2 = pd.crosstab(group1, group2, margins = True)
         num_row = crosstab2.shape[0] - 1
         num_col = crosstab2.shape[1] - 1
         n = crosstab2.iloc[-1, -1]
-
 
 
         ## Creating percentage tables ##
@@ -35,15 +36,15 @@ def crosstab(group1, group2, prop= None, test = False, margins= True,
         crosstab_percell = round(crosstab2.div(crosstab2.iloc[-1,-1], axis=0).mul(100, axis=1), 2)
 
 
-
         ## Chi-square and effect size results ##
-        if correction == None:
+        if correction == None or correction == False:
             correction = False
+
         elif correction == True:
             correction = True
 
         chi2, p, dof, expected = scipy.stats.chi2_contingency(crosstab,
-                                                              correction = correction)
+                                                              correction=correction)
         test_val = chi2
 
         if test == "chi-square":
@@ -55,11 +56,11 @@ def crosstab(group1, group2, prop= None, test = False, margins= True,
         elif test == "g-test":
             test_name = "G-test"
             test_val, p, dof, expected = scipy.stats.chi2_contingency(crosstab,
-                                                              correction = correction,
-                                                              lambda_ = "log-likelihood")
+                                                                      correction=correction,
+                                                                      lambda_="log-likelihood")
         elif test == "mcnemar":
             test_name = "McNemar"
-            results = contingency_tables.mcnemar(crosstab, exact= exact, correction = correction)
+            results = contingency_tables.mcnemar(crosstab, exact=exact, correction=correction)
             test_val = results.statistic
             p = results.pvalue
             dof = 1
@@ -77,7 +78,7 @@ def crosstab(group1, group2, prop= None, test = False, margins= True,
         # Cramer's phi
         # phi = square_root(chi_square / N)
         # Where N = total sample size
-        phi = numpy.sqrt(test_val / n)
+        phi = np.sqrt(test_val / n)
 
         # Cramer's V
         # V = square_root(chi_square / min(c-1, r-1))
@@ -85,13 +86,13 @@ def crosstab(group1, group2, prop= None, test = False, margins= True,
             phi_corrected = (test_val / n) - ((num_row - 1) * (num_col - 1) / (n - 1))
             phi_corrected = max(0, phi_corrected)
 
-            row_corrected = num_row - numpy.square(num_row - 1) / (n - 1)
-            col_corrected = num_col - numpy.square(num_col - 1) / (n - 1)
+            row_corrected = num_row - np.square(num_row - 1) / (n - 1)
+            col_corrected = num_col - np.square(num_col - 1) / (n - 1)
 
-            V = numpy.sqrt(phi_corrected / min((num_row -1), (num_col - 1)))
+            V = np.sqrt(phi_corrected / min((num_row -1), (num_col - 1)))
 
         else:
-            V = numpy.sqrt(test_val / (n * min((num_row - 1), (num_col - 1))))
+            V = np.sqrt(test_val / (n * min((num_row - 1), (num_col - 1))))
 
 
 
@@ -108,70 +109,77 @@ def crosstab(group1, group2, prop= None, test = False, margins= True,
         elif prop == 'cell':
             ct = crosstab_percell
 
-        expected = pandas.DataFrame(expected, index= crosstab.index,
-                             columns= pandas.MultiIndex.from_product([[f"{crosstab.columns.name}"],
-                             crosstab.columns]))
+        expected = pd.DataFrame(expected, index=crosstab.index,
+                                columns=pd.MultiIndex.from_product([[f"{crosstab.columns.name}"], crosstab.columns]))
 
-        ct.columns = pandas.MultiIndex.from_product([[f"{ct.columns.name}"],
-                                                     ct.columns])
+        ct.columns = pd.MultiIndex.from_product([[f"{ct.columns.name}"], ct.columns])
 
 
 
         ## Creating the results table ##
         if test != False:
             if test == "fisher":
-                table = pandas.DataFrame(numpy.zeros(shape= (5,2)),
-                         columns = [f'{test_name}', 'results'])
+                results = {f"{test_name}": [],
+                           "results": []}
 
-                table.iloc[0,0] = f"Odds ratio = "
-                table.iloc[0,1] = round(ods2, 4)
+                results[f"{test_name}"].append(f"Odds ratio = ")
+                results["results"].append(round(ods2, 4))
 
-                table.iloc[1,0] = f"2 sided p-value = "
-                table.iloc[1,1] = round(p2, 4)
+                results[f"{test_name}"].append(f"2 sided p-value = ")
+                results["results"].append(round(p2, 4))
 
-                table.iloc[2,0] = f"Left tail p-value = "
-                table.iloc[2,1] = round(pl, 4)
+                results[f"{test_name}"].append(f"Left tail p-value = ")
+                results["results"].append(round(pl, 4))
 
-                table.iloc[3,0] = f"Right tail p-value = "
-                table.iloc[3,1] = round(pg, 4)
+                results[f"{test_name}"].append(f"Right tail p-value = ")
+                results["results"].append(round(pg, 4))
 
-                table.iloc[4,0] = f"Cramer's phi = "
-                table.iloc[4,1] = round(phi, 4)
+                results[f"{test_name}"].append(f"Cramer's phi = ")
+                results["results"].append(round(phi, 4))
+
+                table = pd.DataFrame.from_dict(results)
 
             elif test != "fisher":
-                table = pandas.DataFrame(numpy.zeros(shape= (3,2)),
-                         columns = [f'{test_name}', 'results'])
+                results = {f"{test_name}": [],
+                           "results": []}
 
                 if test == "chi-square":
-                    table.iloc[0,0] = f"Pearson Chi-square ({dof: .1f}) = "
+                    results[f"{test_name}"].append(f"Pearson Chi-square ({round(dof, 1)}) = ")
+                    results["results"].append(round(test_val, 4))
                 elif test == "g-test":
-                    table.iloc[0,0] = f"Log-likelihood ratio ({dof: .1f}) = "
+                    results[f"{test_name}"].append(f"Log-likelihood ratio ({round(dof, 1)}) = ")
+                    results["results"].append(round(test_val, 4))
                 elif test == "mcnemar":
-                    table.iloc[0,0] = f"McNemar's Chi-square ({dof: .1f}) = "
-                table.iloc[0,1] = round(test_val, 4)
+                    results[f"{test_name}"].append(f"McNemar's Chi-square ({round(dof, 1)}) = ")
+                    results["results"].append(round(test_val, 4))
 
-                table.iloc[1,0] = f"p-value = "
-                table.iloc[1,1] = round(p, 4)
+                results[f"{test_name}"].append("p-value = ")
+                results["results"].append(round(p, 4))
 
                 if crosstab.size == 4:
-                    table.iloc[2,0] = f"Cramer's phi = "
-                    table.iloc[2,1] = round(phi, 4)
+                    results[f"{test_name}"].append("Cramer's phi = ")
+                    results["results"].append(round(phi, 4))
                 elif crosstab.size > 4:
-                    table.iloc[2,0] = f"Cramer's V = "
-                    table.iloc[2,1] = round(V, 4)
+                    results[f"{test_name}"].append("Cramer's V = ")
+                    results["results"].append(round(V, 4))
 
+                table = pd.DataFrame.from_dict(results)
 
 
         ## Returning DataFrame objects ##
         if expected_freqs == False:
             if test == False:
                 return ct
+
             elif test != False:
                 return ct, table
+
         elif expected_freqs == True:
             if test == "mcnemar":
               print("Expected frequencies not appropriate for this test, remove argument and try again.")
+
             elif test == False:
                 return ct, expected
+
             elif test != False:
                 return ct, table, expected
